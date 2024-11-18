d4ca4243dde4a970bc77e30352eef272abe0667d
mr_degama <hardikdegama972@gmail.com>
2024-11-18 17:55:01 +0530

    this ia a them change

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-
-import React   from 'react';
+import React from 'react';
 import Calculator from './Component/CalcuLator';
 import ButtonClickChangeValue from './Component/ButtonClickGetValue';
 import ToggleSwitch from './Component/ChangeThem';
 
 function App() {
   return (
-      <>
-      
-
-    < Calculator />
-  < ButtonClickChangeValue />
-  < ToggleSwitch/>
-      </>
-
+    <>
+      <h1 style={{ color: 'red' }}>IN THIS A OUTPUT A BLACK THEM NOT A WORK IN A useEffect AND UseRef in this a Example.</h1>
+      <Calculator />
+      <ButtonClickChangeValue />
+      <ToggleSwitch />
+    </>
   );
 }
 
